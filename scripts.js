a8d9fbd0caf2203db2135ca11f1f873b33588c10
ParadoxiusBlack <98045278+ParadoxiusBlack@users.noreply.github.com>
2024-10-16 18:02:04 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+document.querySelector('header').addEventListener('click', function() {
+    this.classList.add('jump');
+    // Remove the class after the animation ends to allow re-triggering
+    setTimeout(() => {
+        this.classList.remove('jump');
+    }, 500); // Match the duration of the animation
+});
+
 function showSection(sectionId) {
     const sections = document.querySelectorAll('main section');
     sections.forEach(section => {
