8d91c20c8039d01084b75ddea1d11983248c93df
ParadoxiusBlack <98045278+ParadoxiusBlack@users.noreply.github.com>
2024-12-02 04:50:16 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  initValidation("#myform");
+});
+
+function initValidation(formId) {
+  const form = document.querySelector(formId);
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (validateForm()) {
+      form.style.display = 'none';
+      document.getElementById('thankYouMessage').style.display = 'block';
+    }
+  });
+
+  // Add onChange event listeners for individual fields
+}
